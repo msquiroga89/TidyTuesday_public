7b301842edae9955a8fc85edfc94f7ecfbeb7d45
Macarena <macarenasolquiroga@gmail.com>
2021-02-18 13:16:59 -0300

    week 8

--- 2021_Week 8.R
+++ 2021_Week 8.R
@@ -15,6 +15,46 @@ conjugal <- tuesdata$conjugal
 georgia_pop <- tuesdata$georgia_pop
 ##############################################################################
 
+### Challenge 04
+
+# df longer
+freed_slaves_long <- freed_slaves %>% 
+  pivot_longer(cols = 2:3,
+               names_to="Status",
+               values_to="Percentage")
+
+# percentage calc
+freed_slaves_long <- freed_slaves_long %>% 
+  group_by(Year, Status) %>% 
+  summarise(n = sum(Percentage)) %>% 
+  mutate(Percentage = n / sum(n))
+
+# plot
+ggplot(freed_slaves_long, aes(x=Year, y = Percentage, fill = Status))+
+  geom_area()+
+  labs(
+    title = "PROPORTION OF FREEMEN AND SLAVES AMONG AMERICAN NEGROES . \n PROPORTION DES NÈGRES LIBRES ET DES ESCLAVES EN AMÉRIQUE .",
+    subtitle = "DONE BY ATLANTA UNIVERSITY .",
+    caption = "@_msquiroga for #TidyTuesday, Recreation of W.E.B. DuBois data viz"
+  )+
+  scale_fill_manual(values= c("chartreuse4", "black"))+
+  scale_x_continuous(breaks = round(seq(min(freed_slaves_long$Year), max(freed_slaves_long$Year), by = 10),1),
+                     position = "top") +
+  theme_wsj()+
+  theme(
+    plot.title = element_text(size=18,
+                              hjust=0.5),
+    plot.subtitle = element_text(size = 14,
+                                 hjust=0.5),
+    plot.caption = element_text(size=10,
+                                hjust =0.75),
+    legend.position = "none",
+    axis.title.y = element_blank()      )+
+  annotate("text", x = 1825, y = 0.75, label = "SLAVES \n ESCLAVES", color = "white", fontface = "bold", size = 7)+
+  annotate("text", x = 1825, y = 0.93, label = "FREE - LIBRE", color = "black", fontface = "bold", size = 7)
+
+
+
 ### Challenge 05
 
 # longer df
